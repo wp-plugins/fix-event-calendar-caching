b81824414d63431b9bca7d5b0a3c63bf759b187e
DanielMahaffy <DanielMahaffy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-02 16:45:42 +0000

    Added version 1.0.0 of the plugin
    git-svn-id: https://plugins.svn.wordpress.org/fix-event-calendar-caching/trunk@1081059 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ fix-event-calendar-caching.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Plugin Name: Fix Event Calendar Caching
+ * Description: This plugin makes the All-in-One Event Calendar's javascript cacheable to improve page load speed.
+ * Version: 1.0.0
+ * Author: Daniel Mahaffy
+ * License: GPLv2 or later
+ */
+function fix_event_cal_init() {
+    if (!is_user_logged_in()) {// only use the static script file if no one is logged in
+        if (!function_exists('get_plugins')) {//We need this to check version of the event cal plugin
+            require_once ABSPATH . 'wp-admin/includes/plugin.php';
+        }
+        $pluginDir = __DIR__ . "/..";
+        $pluginName = "all-in-one-event-calendar";
+        $version = get_plugin_data("$pluginDir/$pluginName/$pluginName.php")['Version'];
+
+        //embed the version number of the Event Cal plugin in the static js name
+        $jsFile = __DIR__ . "/event-cal-$version.js";
+        $originalUrl = get_site_url() . "/?ai1ec_render_js=common_frontend&is_backend=false&ver=$version"; //This is where we can load the original dynamic js
+        if (!file_exists($jsFile)) {//first time running or first time since a new version of the event calendar
+            file_put_contents($jsFile, file_get_contents($originalUrl));
+        }
+        if (file_exists($jsFile)) {
+            wp_dequeue_script('ai1ec_requirejs'); //remove the dynamic js script
+            wp_enqueue_script('event_cal_replace', plugins_url("event-cal-$version.js", __FILE__), array(), null, true); //add our static js
+        }
+    }
+}
+
+/**
+ * Call our funciton.  The event cal plugin doesn't enqueue it's script until 
+ * the wp_footer, so we need to use that action too.
+ */
+add_action('wp_footer', 'fix_event_cal_init', 19);
---
+++ readme.txt
@@ -0,0 +1,30 @@
+=== Plugin Name ===
+Contributors: (this should be a list of wordpress.org userid's)
+Tags: calendar, javascript, cache
+Requires at least: 4.1
+Tested up to: 4.1
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin makes the All-in-One Event Calendar's javascript cacheable to improve page load speed.
+
+== Description ==
+
+The "All-in-One Event Calendar" by Time.ly works great, but uses dynamic javascript.
+This plugin creates a static version of the javascript that can be cached by
+the browser. 
+
+I have tested this plugin with version 2.1.9 of the event calendar plugin.  
+It may work with future releases, but use it at your own risk.
+
+This plugin only uses the static javascript on the frontend.  If a user is 
+logged in, the original dynamic script will be used.  As far as I can tell
+caching the javascript does not interfere with the plugin's functionality, but
+there may be features of the event calendar that do not work with this plugin.
+
+== Installation ==
+
+1. Upload the fix-event-calendar-caching directory to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. The plugin needs write access to it's folder to create the static js file.
